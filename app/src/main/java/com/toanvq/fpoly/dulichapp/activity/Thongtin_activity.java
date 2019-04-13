b41180a9fee52e3c5bad651d1c8e692632c574db
toanvq <toankieu001@gmail.com>
2019-04-13 16:12:02 +0700

    du lich

---
+++ app/src/main/java/com/toanvq/fpoly/dulichapp/activity/Thongtin_activity.java
@@ -0,0 +1,15 @@
+package com.toanvq.fpoly.dulichapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.toanvq.fpoly.dulichapp.R;
+
+public class Thongtin_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.thongtin_activity);
+    }
+}
